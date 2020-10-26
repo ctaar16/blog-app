556d9d0fb804ccaade73491f887db415056f4b01
omegadesigner <omegadesigner@hotmail.com>
2020-10-26 17:10:40 -0400

    reportWebVitals fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import reportWebVitals from "./reportWebVitals"
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
